b8f26fbc5f189f46a7e7c58fe62625be6f52e688
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-10-12 19:02:22 +0300

    use flask-login to handel auth use and create base.html

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-
-
+from flask_login import LoginManager
 app = Flask(__name__)
 app.secret_key = "2e12d23e-783a-4430-82ad-5c2f6713b4a4"
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from src import views
