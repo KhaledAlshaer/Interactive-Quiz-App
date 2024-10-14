1f565cdea971594927241ed10f4de7570c477aef
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-10-14 16:23:31 +0300

    fixed username field in the login form

--- src/models/forms.py
+++ src/models/forms.py
@@ -32,7 +32,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField(validators=[DataRequired()])
     submit = SubmitField("Log In")
 
