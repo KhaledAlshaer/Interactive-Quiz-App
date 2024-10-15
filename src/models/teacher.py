f9d8d9ff8a640981b8fb209b4c0585735729a093
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-10-15 13:31:35 +0300

    initialized teacher class

---
+++ src/models/teacher.py
@@ -0,0 +1,10 @@
+import bcrypt
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from user import User
+
+
+class Teacher(User):
+    """
+    """
+    pass
